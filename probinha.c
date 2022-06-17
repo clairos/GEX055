2da3985969a20bf1de981034453e39b840c593c7
Jonathan Gotz Correa <jonathan.correa@contaseg.local>
2022-06-17 14:48:06 -0300

    função 1 e 2 ok

--- probinha.c
+++ probinha.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//#include <math. h> 
 
 int comb(int n, int x, int ene, int xis){
     //começo da conta de Combinação
@@ -31,21 +32,44 @@ int comb(int n, int x, int ene, int xis){
     return 0;    
 }
 
+int pex(float p, float P, int xis){
+    
+    int seq;
+    float expo;
+    expo = P;
+    for(seq = 0; seq < (xis - 1); seq++){
+        expo *= P;
+    }
+        printf("p ^ x = %.5f\n", expo);
+        return expo;
+}
+
+
+int qnx(float p, float P, int xis, int ene){
+    
+}
+
 int main(){
  
-    int ene, xis, nx, n, x;
-    float p, combinacao;
+    int ene, xis, xiss, nx, n, x, combinacao;
+    float p, P, PEX, QNX;
 
     //pedindo os valores de n e x
     printf("Digite o número de observações: \n");
     scanf("%d", &ene);
-    // printf("Digite a probabilidade de sucesso: \n");
-    // scanf("%f", &p);
+    printf("Digite a probabilidade de sucesso: \n");
+    scanf("%f", &p);
     printf("Digite o número especificado de sucesso: \n");
     scanf("%d", &xis);
     n = ene;
     x = xis;
+    P = p;
+    //chamando funçao combinação
     combinacao = comb(n, x, ene, xis);
+    //chamando função de calcular o P elevado a X
+    PEX = pex(p, P, xis);
+    //chamando função de calcular o q elavado a n - x
+    QNX = qnx(p, P, xis, ene);
 
     return 0;
 }
